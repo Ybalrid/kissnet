d237f69555e2f3ba1adfd4f1df8c59d020243c5a
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-09-28 18:03:29 +0200

    fixed that error handler test

--- tests/error_handler_test/main.cpp
+++ tests/error_handler_test/main.cpp
@@ -5,18 +5,15 @@ int main()
 {
 	kissnet::error::abortOnFatalError = false;
 
-	kissnet::error::handler("test what code would be called on error when built without exception\n");
+	kissnet::error::handle("test what code would be called on error when built without exception\n");
 
-	kissnet::error::callback = [](const std::string& str, void* ctx)
-	{
+	kissnet::error::callback = [](const std::string& str, void* ctx) {
 		std::cerr << "this is the callback : ";
 		std::cerr << str;
 		(void)ctx;
 	};
 
+	kissnet::error::handle("test our custom callback");
 
-	kissnet::error::handler("test our custom callback");
-
-
-    return 0;
+	return 0;
 }
