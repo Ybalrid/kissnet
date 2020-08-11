e260131d5abb21aa28bc8a9ff7ec2882d96608aa
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-11 14:34:42 -0400

    allow build with or without OpenSSL

--- kissnet.cpp
+++ kissnet.cpp
@@ -1,7 +1,10 @@
-
-#define KISSNET_USE_OPENSSL
 #include "kissnet.hpp"
 
 int main()
 {
+  #if defined(KISSNET_USE_OPENSSL)
+    printf("kissnet is using OpenSSL.");
+  #else
+    printf("kissnet is not using OpenSSL.");
+  #endif
 }
